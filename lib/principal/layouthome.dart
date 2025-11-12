bd4234f5c6ef08d6e935f0a05e0a80a87d0cc9e7
Oscar Mateu <oscaralejandromateu@gmail.com>
2025-11-12 17:14:47 +0100

    Adición de menu de mapa y lista sin funcionalidad

--- lib/principal/layouthome.dart
+++ lib/principal/layouthome.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'mapa.dart';
 
-class Layouthome extends StatelessWidget {
+class Layouthome extends StatefulWidget {
   const Layouthome({super.key});
+
+  @override
+  State<Layouthome> createState() => _LayouthomeState();
+}
+
+class _LayouthomeState extends State<Layouthome> {
+  bool _showMap = true;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +21,7 @@ class Layouthome extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
-                color: Color(0xFFFF9350),
+                color: const Color(0xFFFF9350),
                 borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(20),
                   bottomRight: Radius.circular(20),
@@ -28,6 +35,53 @@ class Layouthome extends StatelessWidget {
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFF9350),
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: ToggleButtons(
+                            isSelected: [_showMap, !_showMap],
+                            onPressed: (index) {
+                              setState(() {
+                                _showMap = index == 0;
+                              });
+                            },
+                            borderRadius: BorderRadius.circular(8),
+                            selectedColor: Colors.black,
+                            color: Colors.white,
+                            fillColor: Colors.white70,
+                            constraints: const BoxConstraints(
+                                minHeight: 36, minWidth: 100),
+                            children: const [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8),
+                                child: Text(
+                                  'Mapa',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8),
+                                child: Text(
+                                  'Lista',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                   Row(
                     children: [
                       Expanded(
@@ -53,7 +107,6 @@ class Layouthome extends StatelessWidget {
                 ],
               ),
             ),
-
             Expanded(
               child: Container(
                 margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
@@ -61,18 +114,16 @@ class Layouthome extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(12),
                 ),
-                // Incrustar el mapa aquí
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: const MapWidget(),
+                  child: _showMap ? const MapWidget() : const Placeholder(), // Cambia entre mapa y placeholder
                 ),
               ),
             ),
-
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               decoration: BoxDecoration(
-                color: Color(0xFFFF9350),
+                color: const Color(0xFFFF9350),
                 borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
