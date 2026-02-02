8492d2bac436f5f4d374e8324134574ea9af0d0e
Oscar <oscmattri@alu.edu.gva.es>
2026-02-02 16:37:35 +0100

    arreglo pequeño

--- lib/Implementaciones/gasolineras/presentacion/widgets/filter_dialogs/fuel_filter_dialog.dart
+++ lib/Implementaciones/gasolineras/presentacion/widgets/filter_dialogs/fuel_filter_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_gasolinera/l10n/app_localizations.dart';
+import 'package:my_gasolinera/core/l10n/app_localizations.dart';
 
 class FuelFilterDialog extends StatefulWidget {
   final String? valorActual;
